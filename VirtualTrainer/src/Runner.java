32d2b0507d958a79f728028a5ff4c4e05314b25b
WAC4173 <43006321+WAC4173@users.noreply.github.com>
2018-11-07 06:58:52 -0500

    Add files via upload

---
+++ VirtualTrainer/src/Runner.java
@@ -0,0 +1,21 @@
+
+public class Runner {
+	Corner wgo;
+	Corner wbo;
+	Corner wbr;
+	Corner wgr;
+	Corner ygo;
+	Corner ybo;
+	Corner ybr;
+	Corner ygr;
+	public Runner(Corner wgo ,Corner wbo, Corner wbr, Corner wgr, Corner ygo, Corner ybo, Corner ybr, Corner ygr) {
+		this.wgo = wgo;
+		this.wbo = wbo;
+		this.wbr = wbr;
+		this.wgr = wgr;
+		this.ygo = ygo;
+		this.ybo = ybo;
+		this.ybr = ybr;
+		this.ygr = ygr;		
+	}
+}
